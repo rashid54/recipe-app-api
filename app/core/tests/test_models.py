fde625f64f56463fa2b0c5c246ab44c023af547c
rashid <rashid28hasan@gmail.com>
2020-04-08 20:30:52 +0600

    added ImageField to recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,4 +1,7 @@
+from unittest.mock import patch
+
 from core import models
+
 from django.db.utils import IntegrityError
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -76,3 +79,13 @@ class ModelTests(TestCase):
             price=5.90
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_imagefile_uuid(self, mock_uuid):
+        """Test that image is in the correct location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'mytestimage.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
