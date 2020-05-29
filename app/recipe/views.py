2c163e30373398fb29eb1183b6579b899e71f6c0
rashid <rashid28hasan@gmail.com>
2020-05-30 00:01:16 +0600

    added tag and ingredient filter by assigned only feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,7 +18,12 @@ class TagViewSet(viewsets.GenericViewSet,
     serializer_class = TagSerializer
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(self.request.query_params.get('assigned_only'))
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -34,7 +39,12 @@ class IngredientViewSet(viewsets.GenericViewSet,
     serializer_class = IngredientSerializer
 
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(self.request.query_params.get('assigned_only'))
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
